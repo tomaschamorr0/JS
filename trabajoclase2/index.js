5f1ac235782a8013c88084d56fedd9c9296a3683
tomaschamorr0 <111659471+tomaschamorr0@users.noreply.github.com>
2022-08-20 22:33:25 -0300

    Add files via upload

---
+++ trabajoclase2/index.js
@@ -0,0 +1,26 @@
+let nombre = prompt("ingrese su nombre");
+while (nombre =="") {
+    alert("Nombre no Identificado");
+    nombre = prompt("ingrese su nombre");
+
+}
+    alert(`Bienvenid@ ${nombre}`);
+let numero = parseInt(prompt("ingrese un numero del 1 al 10"));
+do{    
+    if (numero>=1 && numero<=10){
+        alert(`La tabla del ${numero} es`);
+        for (let i = 1; i <= 10; i++){
+            let multiplicacion = numero * i;
+            let mensaje = `${numero} * ${i} =${multiplicacion}`;
+            alert(mensaje);  
+        }   
+    }
+    numero = prompt("ingresar un numero del 1 al 10");
+}
+while(numero<=10 && numero>=1);
+alert(`El numero ${numero} es mayor a 10`);
+let siwhile = prompt("¿Te gusto?").toLowerCase();
+while (siwhile != "si") {
+    siwhile = prompt("No entendi, ¿Te gusto?");
+}
+alert("Me alegro de que te haya gustado");
